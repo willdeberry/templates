0820473c14e1971e1acfa2a8eca020b3e92c6789
Francis Ferrell <fferrell@getwellnetwork.com>
2016-12-14 16:21:10 -0500

    add post-installation instructions

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,3 +4,24 @@ if ! [[ -e ./debian/changelog ]] ; then
 	dch --create --package {{ cookiecutter.debian_package_name }} --newversion 0.0.1
 fi
 
+
+cat <<EOF
+
+
+
+Awesome!
+
+Here's what to do next:
+ 1. Go into gitlab
+ 2. Create a project named '{{ cookiecutter.repository_name }}'
+ 3. Come back here
+ 4. Run these commands:
+    cd {{ cookiecutter.repository_name }}
+    git init
+    git add .
+    git commit -m 'initial commit'
+    git remote add origin git@git.internal.getwellnetwork.com:plcdev/{{ cookiecutter.repository_name }}.git
+    git push -u origin master
+
+EOF
+
