9b9b90757ee2eba4bfeabb719be798e77a0dd2de
Francis Ferrell <fferrell@getwellnetwork.com>
2016-12-15 20:39:12 -0500

    fix typo

--- {{cookiecutter.repository_name}}/gwn/{{cookiecutter.python_subpackage_name}}/foo_service.py
+++ {{cookiecutter.repository_name}}/gwn/{{cookiecutter.python_subpackage_name}}/foo_service.py
@@ -12,7 +12,7 @@ class FooService( dbus.service.Object ):
 
     def __init__( self ):
         bus_name = dbus.service.BusName( BUSNAME, bus = dbus.SystemBus() )
-        super().__init__( bus_name = bus_name, object_path = OBJECTPATH)
+        super().__init__( bus_name = bus_name, object_path = OBJECTPATH )
         logger.info( 'connected to system bus' )
 
 
