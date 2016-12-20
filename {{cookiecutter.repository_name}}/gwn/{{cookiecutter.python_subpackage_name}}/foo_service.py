5eb0879dfc02ec8c644ae1aa6c678ebfd5e8e325
Francis Ferrell <fferrell@getwellnetwork.com>
2016-12-20 08:22:00 -0500

    switch example service to session bus

--- {{cookiecutter.repository_name}}/gwn/{{cookiecutter.python_subpackage_name}}/foo_service.py
+++ {{cookiecutter.repository_name}}/gwn/{{cookiecutter.python_subpackage_name}}/foo_service.py
@@ -11,7 +11,7 @@ from .foo import foo
 class FooService( dbus.service.Object ):
 
     def __init__( self ):
-        bus_name = dbus.service.BusName( BUSNAME, bus = dbus.SystemBus() )
+        bus_name = dbus.service.BusName( BUSNAME, bus = dbus.SessionBus() )
         super().__init__( bus_name = bus_name, object_path = OBJECTPATH )
         logger.info( 'connected to system bus' )
 
